cbdc910bc142de88814400d0b79225a6ce5c39fb
tenshi <alexander.pilipenko@iage.net>
2016-11-22 13:37:03 +0300

    Always set required native fields

--- native_markup.go
+++ native_markup.go
@@ -10,15 +10,15 @@ type NativeAdMarkup struct {
 
 type NativeAdMarkupInner struct {
 	Ver         int              `json:"ver,omitempty"`
-	Assets      []NativeAsset    `json:"assets,omitempty"`
-	Link        NativeLink       `json:"link,omitempty"`
+	Assets      []NativeAsset    `json:"assets"`
+	Link        NativeLink       `json:"link"`
 	ImpTrackers []string         `json:"imptrackers,omitempty"`
 	JsTracker   string           `json:"jstracker,omitempty"`
 	Ext         *json.RawMessage `json:"ext,omitempty"`
 }
 
 type NativeAsset struct {
-	ID       int              `json:"id,omitempty"`
+	ID       int              `json:"id"`
 	Required int              `json:"required,omitempty"`
 	Title    *NativeTitle     `json:"title,omitempty"`
 	Image    *NativeImage     `json:"img,omitempty"`
@@ -29,30 +29,30 @@ type NativeAsset struct {
 }
 
 type NativeData struct {
-	Value string           `json:"value,omitempty"`
+	Value string           `json:"value"`
 	Label string           `json:"label,omitempty"`
 	Ext   *json.RawMessage `json:"ext,omitempty"`
 }
 
 type NativeImage struct {
-	URL string           `json:"url,omitempty"`
+	URL string           `json:"url"`
 	H   int              `json:"h,omitempty"`
 	W   int              `json:"w,omitempty"`
 	Ext *json.RawMessage `json:"ext,omitempty"`
 }
 
 type NativeLink struct {
-	URL           string           `json:"url,omitempty"`
+	URL           string           `json:"url"`
 	ClickTrackers []string         `json:"clicktrackers,omitempty"`
 	Fallback      string           `json:"fallback,omitempty"`
 	Ext           *json.RawMessage `json:"ext,omitempty"`
 }
 
 type NativeTitle struct {
-	Text string           `json:"text,omitempty"`
+	Text string           `json:"text"`
 	Ext  *json.RawMessage `json:"ext,omitempty"`
 }
 
 type NativeVideo struct {
-	VASTTag string `json:"vasttag,omitempty"`
+	VASTTag string `json:"vasttag"`
 }
